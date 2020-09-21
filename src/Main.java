8705d6885a33f538c13793276b50edf7f6190fba
JRichardson95 <71064586+JRichardson95@users.noreply.github.com>
2020-09-21 11:22:55 +0200

    Klasse toegevoegd object aangemaakt

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Person e = new Person("Jay");
         System.out.println(e.getName());
+        System.out.println(e.getName() + " R");
     }
 }
